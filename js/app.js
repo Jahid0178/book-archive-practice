0452eba11755381d25ef2f50c9689d2c3c837019
Jahid0178 <Jahidulhaque767@gmail.com>
2021-09-03 01:20:21 +0600

    added some files

--- js/app.js
+++ js/app.js
@@ -6,12 +6,23 @@ const spinnerLoad = spinner => {
 const searchBook = () => {
     const searchText = searchField.value;
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayResult(data));
-    
-    searchField.value = '';
-    spinnerLoad('block');
+    if (searchText == 0) {
+        const errorMessage = document.getElementById('error-message');
+        const p = document.createElement('p');
+        p.style.padding = '10px';
+        p.style.fontSize = '18px';
+        p.innerText = `Search your books name`;
+        errorMessage.appendChild(p);
+    } else {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => displayResult(data));
+
+        searchField.value = '';
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = '';
+        spinnerLoad('block');
+    };    
 };
 
 const displayResult = books => {
@@ -34,6 +45,5 @@ const displayResult = books => {
         `;
         displayContainer.appendChild(div);
     });
-    spinnerLoad('none')
+    spinnerLoad('none');
 };
-
